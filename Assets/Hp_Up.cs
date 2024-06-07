43f5777906e08aa4a4271d29f13b5db5136170aa
Andrewyim55 <andrew.yim55@gmail.com>
2024-06-08 01:02:54 +0800

    Created a health pickup with collision on it (Removed the player collision box to avoid conflict on other branch)

--- Assets/Hp_Up.cs
+++ Assets/Hp_Up.cs
@@ -2,17 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Hp_Up : MonoBehaviour
+public class HP_up : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+  // HP to be implemented afterwards (this needs to be updated)
+    private void OnTriggerEnter2D(Collider2D other){
+        if (other.tag == "Player") {
+            print("health up");
+            gameObject.SetActive(false);
+        }
     }
 }
