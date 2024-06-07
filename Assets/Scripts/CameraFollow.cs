88f66783dadc5c48b174dbde4486700541888e8d
bigaboyzs <wongzhengyu@gmail.com>
2024-06-07 23:54:59 +0800

    Added Movement and Camera Follow

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private Transform target;
+
+    [Header("Attributes")]
+    [SerializeField] private float smoothTime;
+
+    private Vector3 offset = new Vector3 (0, 0, -10f);
+    private Vector3 velocity = Vector3.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 targetPos = target.position + offset;
+        transform.position = Vector3.SmoothDamp(transform.position, targetPos, ref velocity, smoothTime);
+    }
+}
