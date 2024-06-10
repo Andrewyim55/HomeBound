184a461bfa39b4ba1df6258df7e90328fe8608e8
bigaboyzs <wongzhengyu@gmail.com>
2024-06-10 14:22:48 +0800

    PowerUps

---
+++ Assets/Scripts/PowerUps.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUps : MonoBehaviour
+{
+    enum powerUp
+    {
+        health,
+        speed,
+        bomb
+    }
+    [Header("Attributes")]
+    [SerializeField] private powerUp type;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            switch (type)
+            {
+            case powerUp.health:
+                    Debug.Log("Health Pick Up");
+                    break;
+            case powerUp.speed:
+                    Debug.Log("Speed Pick Up");
+                    break;
+            case powerUp.bomb:
+                    Debug.Log("Bomb Pick Up");
+                    break;
+            }
+            gameObject.SetActive(false);
+        }
+    }
+}
