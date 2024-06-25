9625f9b3fb8523ecdc09d59abe901ec4e2b72807
WhiteCl0ud01 <136679162+WhiteCl0ud01@users.noreply.github.com>
2024-06-25 14:59:34 +0800

    XP Bar, Health Bar and Weapon Icon
    Left click to see the changes

---
+++ Assets/Scripts/WeaponChanger.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WeaponChanger : MonoBehaviour
+{
+    public Image imageDisplay;
+    public Sprite[] pictures;
+    private int currentPictureIndex = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(pictures.Length > 0)
+        {
+            imageDisplay.sprite = pictures[0];
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            currentPictureIndex = (currentPictureIndex + 1) % pictures.Length;
+            imageDisplay.sprite = pictures[currentPictureIndex];
+            
+        }
+    }
+}
---
+++ Assets/Scripts/XPNLevel.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class XPNLevel : MonoBehaviour
+{
+    public Text XPText;
+    public Text LevelText;
+    public Image xpBarImage;
+    private int XPLevel = 1;
+    private float currentXP;
+    private float maxExperience = 100;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentXP = 0;
+        UpdateXPBar();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            gainXP(10);
+            print(currentXP);
+        }
+    }
+    public void gainXP(float experience)
+    {
+
+        currentXP += experience;
+        if (currentXP >= maxExperience)
+        {
+            currentXP = 0;
+            XPLevel += 1;
+            LevelText.text = XPLevel.ToString();
+        }
+        currentXP = Mathf.Clamp(currentXP, 0, maxExperience);
+        UpdateXPBar(); 
+    }
+
+    private void UpdateXPBar()
+    {
+        XPText.text = currentXP.ToString() + " / 100";
+        float fillAmount = currentXP / maxExperience; // Calculate the fill amount as a fraction of current health over max health
+        xpBarImage.fillAmount = fillAmount; // Set the fill amount of the health bar image
+    }
+}
