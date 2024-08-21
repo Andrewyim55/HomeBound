ecdadb6270fbb39013a575d60f021947e2ab8067
Biyuyu <vealtique@gmail.com>
2024-08-21 16:53:45 +0800

    Change breakable sprite upon breaking

--- Assets/Scripts/Breakables/Breakables.cs
+++ Assets/Scripts/Breakables/Breakables.cs
@@ -5,12 +5,17 @@ public class Breakables : MonoBehaviour
 {
     public event Action OnBreak;
     private Loot loot;
+    private SpriteRenderer spriteRenderer;
     private bool hasBroken = false; //flag to check if already broken
+
+    [SerializeField] private Sprite brokenSprite; 
+    [SerializeField] private float destroyDelay = 0.5f;
 
     // Start is called before the first frame update
     void Start()
     {
         loot = GetComponent<Loot>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -31,12 +36,19 @@ public class Breakables : MonoBehaviour
         hasBroken = true;
         Debug.Log("Breakable broken");
 
+        // replace sprite upon breaking
+        if (spriteRenderer != null && brokenSprite != null)
+        {
+            spriteRenderer.sprite = brokenSprite;
+        }
+
         // drop loot
         if (loot != null)
         {
             loot.DropLoot();
         }
+
         OnBreak?.Invoke(); 
-        Destroy(gameObject);
+        Destroy(gameObject, destroyDelay);
     }
 }
