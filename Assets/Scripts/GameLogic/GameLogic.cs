57e47a1030e4034327526c4c39e45a6a943d3311
bigaboyzs <wongzhengyu@gmail.com>
2024-09-05 19:17:14 +0800

    Added a scene transition effect

--- Assets/Scripts/GameLogic/GameLogic.cs
+++ Assets/Scripts/GameLogic/GameLogic.cs
@@ -8,6 +8,7 @@ using UnityEngine.SceneManagement;
 public class GameLogic : MonoBehaviour
 {
     [SerializeField] public AudioClip victoryClip;
+    [SerializeField] private Animator anim;
     public static GameLogic instance;
     public float gameTime;
     public float timeToBoss;
@@ -31,72 +32,82 @@ public class GameLogic : MonoBehaviour
     }
 
     // 1 is main mene, 2 is sample scene, 3 is boss fight, 4 is tutorial
-    public void ChangeScene(int sceneNum)
+    public IEnumerator ChangeScene(int sceneNum)
     {
         switch (sceneNum)
         {
             case 1:
+                anim.SetTrigger("Start");
+                GetComponent<EnemySpawner>().enabled = false;
+                GetComponent<BreakablesSpawner>().enabled = false;
+                GetComponent<DifficultyManager>().enabled = false;
+                isBossScene = false;
+                isInGame = false;
+                yield return new WaitForSecondsRealtime(anim.GetCurrentAnimatorStateInfo(0).length);
+                SceneManager.LoadSceneAsync("Main Menu");
                 SoundManager.instance.PlayBgm(0);
-                SceneManager.LoadScene("Main Menu");
+                anim.SetTrigger("End");
                 break;
             case 2:
+                anim.SetTrigger("Start");
+                yield return new WaitForSecondsRealtime(anim.GetCurrentAnimatorStateInfo(0).length);
+                SceneManager.LoadSceneAsync("SampleScene");
                 SoundManager.instance.PlayBgm(1);
-                SceneManager.LoadScene("SampleScene");
+                if (Player.instance != null)
+                {
+                    Destroy(Player.instance.gameObject);
+                }
+                yield return new WaitForSecondsRealtime(anim.GetCurrentAnimatorStateInfo(0).length);
+                anim.SetTrigger("End");
+                GetComponent<EnemySpawner>().enabled = true;
+                GetComponent<BreakablesSpawner>().enabled = true;
+                GetComponent<DifficultyManager>().enabled = true;
+                GetComponent<DifficultyManager>().Reset();
+                Time.timeScale = 1f;
+                GetComponent<BreakablesSpawner>().breakablesInScene.Clear();
+                gameTime = 0f;
+                isBossScene = false;
+                isInGame = true;
                 break;
             case 3:
+                anim.SetTrigger("Start");
+                GetComponent<EnemySpawner>().enabled = false;
+                GetComponent<DifficultyManager>().enabled = false;
+                GetComponent<BreakablesSpawner>().breakablesInScene.Clear();
+                yield return new WaitForSecondsRealtime(anim.GetCurrentAnimatorStateInfo(0).length);
+                SceneManager.LoadSceneAsync("Boss Fight");
                 SoundManager.instance.PlayBgm(2);
-                SceneManager.LoadScene("Boss Fight");
+                anim.SetTrigger("End");
+                Player.instance.transform.position = new UnityEngine.Vector3(3.8f, -3f, 0);
                 break;
             case 4:
+                anim.SetTrigger("Start");
+                yield return new WaitForSecondsRealtime(anim.GetCurrentAnimatorStateInfo(0).length);
+                SceneManager.LoadSceneAsync("Tutorial");
                 SoundManager.instance.PlayBgm(0);
-                SceneManager.LoadScene("Tutorial");
+                anim.SetTrigger("End");
                 break;
         }
     }
 
     public void NewGame()
     {
-        SoundManager.instance.PlayBgm(1);
-        if (Player.instance != null)
-        {
-            Destroy(Player.instance.gameObject);
-        }
-        ChangeScene(2);
-        GetComponent<EnemySpawner>().enabled = true;
-        GetComponent<BreakablesSpawner>().enabled = true;
-        GetComponent<DifficultyManager>().enabled = true;
-        GetComponent<DifficultyManager>().Reset();
-        Time.timeScale = 1f;
-        GetComponent<BreakablesSpawner>().breakablesInScene.Clear();
-        gameTime = 0f;
-        isBossScene = false;
-        isInGame = true;
+        StartCoroutine(ChangeScene(2));
     }
 
     public void MainMenu()
     {
-        SoundManager.instance.PlayBgm(0);
-        ChangeScene(0);
         if (Player.instance != null)
         {
             Destroy(Player.instance.gameObject);
         }
-        ChangeScene(1);
-        GetComponent<EnemySpawner>().enabled = false;
-        GetComponent<BreakablesSpawner>().enabled = false;
-        GetComponent<DifficultyManager>().enabled = false;
-        isBossScene = false;
-        isInGame = false;
+        StartCoroutine(ChangeScene(1));
     }
 
     // function will change to the boss scene
     public void BossScene()
     {
-        GetComponent<EnemySpawner>().enabled = false;
-        GetComponent<DifficultyManager>().enabled = false;
-        GetComponent<BreakablesSpawner>().breakablesInScene.Clear();
-        ChangeScene(3);
-        Player.instance.transform.position = new UnityEngine.Vector3(3.8f, -3f, 0);
+        StartCoroutine(ChangeScene(3));
     }
 
     private void Update()
