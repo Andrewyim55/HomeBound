2e3720c274bf4680bb91e4e65956eb327227aabb
bigaboyzs <wongzhengyu@gmail.com>
2024-09-08 03:43:24 +0800

    transition update

--- Assets/Scripts/GameLogic/GameLogic.cs
+++ Assets/Scripts/GameLogic/GameLogic.cs
@@ -74,7 +74,6 @@ public class GameLogic : MonoBehaviour
                     Destroy(Player.instance.gameObject);
                 }
                 SetPaused(true);
-                anim.SetTrigger("End");
                 yield return new WaitForSecondsRealtime(1f);
                 SetPaused(false);
                 GetComponent<EnemySpawner>().enabled = true;
@@ -152,7 +151,7 @@ public class GameLogic : MonoBehaviour
 
         if (Player.instance != null)
         {
-            if(Player.instance.getStatus())
+            if(Player.instance.getStatus() && !isSceneChanging)
             {
                 gameTime += Time.deltaTime;
             }
@@ -188,7 +187,9 @@ public class GameLogic : MonoBehaviour
     IEnumerator ToGameScene()
     {
         SoundManager.instance.PlaySfx(startGameClip, transform);
-        yield return new WaitForSeconds(startGameClip.length);
+        yield return new WaitForSeconds(startGameClip.length / 2);
+        anim.SetTrigger("End");
+        yield return new WaitForSeconds(anim.GetCurrentAnimatorStateInfo(0).length);
         isSceneChanging = false;
     }
 }
